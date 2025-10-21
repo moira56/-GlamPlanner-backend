bb24ec9f5887861b0a1125564e6638bd8d62f4f3
moira56 <mcekada1@student.unipu.hr>
2025-10-21 20:25:34 +0200

    Povezivanje - CORS

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import { connectToDatabase } from "./db.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 let db = await connectToDatabase();
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend radi i CORS je OK" });
+});
 
 let PORT = 3000;
 
