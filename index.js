f2bf358ab11a00677b44fed54da31532d9e06b15
moira56 <mcekada1@student.unipu.hr>
2025-12-08 20:17:59 +0100

    CORS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://glamplanner.netlify.app" }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
